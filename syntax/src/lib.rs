cac0065af4e813c67a0530474332c04b77efb679
Scetch <lucierbrandon@gmail.com>
2017-06-29 13:01:36 -0400

    Fixed error message escaped strings.

--- syntax/src/lib.rs
+++ syntax/src/lib.rs
@@ -158,8 +158,8 @@ pub fn format_parse_error<'a, I, W, S>(w: W, source: I, err: ParseError<S>) -> R
                             token_len = m.len();
                         },
                         Info::Token(t) => {
-                            write!(&mut msg, "{}", t.escape_default())?;
-                            write!(&mut label, "{}", t.escape_default())?;
+                            write!(&mut msg, "{}", t)?;
+                            write!(&mut label, "{}", t)?;
                         },
                         _ => unimplemented!(),
                     },
@@ -183,7 +183,7 @@ pub fn format_parse_error<'a, I, W, S>(w: W, source: I, err: ParseError<S>) -> R
                             msg.push_str(m);
                         },
                         Info::Token(t) => {
-                            write!(&mut msg, "{}", t.escape_default())?;
+                            write!(&mut msg, "{}", t)?;
                         },
                         _ => unimplemented!(),
                     },
